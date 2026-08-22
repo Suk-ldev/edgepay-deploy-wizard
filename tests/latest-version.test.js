bcbbe2dc7a77271ba26d2b504b72984b73e70e95
Suk-ldev <1603913438@qq.com>
2026-08-22 22:35:20 +0800

    chore: pin payment worker v1.1.1

--- tests/latest-version.test.js
+++ tests/latest-version.test.js
@@ -3,9 +3,9 @@ import test from 'node:test';
 import { handleLatestVersion } from '../src/latest-version-handler.js';
 
 const valid = {
-  TEMPLATE_VERSION: '1.1.0',
-  TEMPLATE_COMMIT_SHA: 'c381279f251c2f3e0ca08051a4246981e5a196ee',
-  TEMPLATE_ENTRY_SHA256: '2d56bff850e1ec853b523e720ab24381770e65affea1af465dd0da3bbf0efd8b',
+  TEMPLATE_VERSION: '1.1.1',
+  TEMPLATE_COMMIT_SHA: '477f86492b9133d2f247f25e97957e1330c4e4d1',
+  TEMPLATE_ENTRY_SHA256: 'a4b77b4c480855061b843a65377e042901fc8d44b1a1060587c7f44e7dd5f867',
 };
 
 test('公开版本接口返回部署向导锁定的商业发行版本', async () => {
@@ -16,7 +16,7 @@ test('公开版本接口返回部署向导锁定的商业发行版本', async ()
     ok: true,
     name: 'edgepay-commercial-worker',
     edition: 'public-commercial-encrypted',
-    version: '1.1.0',
+    version: '1.1.1',
     commit: valid.TEMPLATE_COMMIT_SHA,
     sha256: valid.TEMPLATE_ENTRY_SHA256,
   });
